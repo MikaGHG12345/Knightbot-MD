5f9b2276b3e1232d5572645fc748db6a9c82fa8b
MikaGHG12345 <mika.stiller@gmx.de>
2025-07-22 21:30:57 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "BYT BOT",
   botOwner: 'BYT', // Your name
-  ownerNumber: '4915121347674', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '4916238984779', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
